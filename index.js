b129bdcf0564ddeccedd22f175d85e4df2750ab2
paulanny <paulanimashaun20@gmail.com>
2022-11-01 22:48:08 +0100

    firstpageresponsive

--- index.js
+++ index.js
@@ -8,15 +8,40 @@
 //     PlusIcon.style.transform = `translateX(90deg)`;
 // })
 
-const targetDiv = document.getElementById("tablepss");
-    const smallp = document.getElementById("smallp");
-    const plusicon = document.getElementById("plusiconn");
-    tablepss.onclick = function () {
-      if (smallp.style.display !== "none") {
-        smallp.style.display = "none";
-        plusicon.style.transform = `rotate(0deg)`;
-      } else {
-        smallp.style.display = "block";
-        plusicon.style.transform = `rotate(90deg)`;
-      }
-    };
+// const targetDiv = document.getElementById("tablepss");
+    // const smallp = document.getElementById("smallp");
+    // const plusicon = document.getElementById("plusiconn");
+    // tablepss.onclick = function () {
+    //   if (smallp.style.display !== "none") {
+    //     smallp.style.display = "none";
+    //     plusicon.style.transform = `rotate(0deg)`;
+    //   } else {
+    //     smallp.style.display = "block";
+    //     plusicon.style.transform = `rotate(90deg)`;
+    //   }
+    // };
+
+    const navMenu = document.querySelector(".tableps");
+    const plusicon = document.querySelector(".plusicon");
+    const tab1smallp = document.querySelector(".tab1smallp");
+
+    document.querySelectorAll(".tableps").forEach(n => n.
+        addEventListener("click", () => {
+            if (tab1smallp.style.display !== "none") {
+                tab1smallp.style.display = "none";
+                plusicon.style.transform = `rotate(0deg)`;
+              } else {
+                tab1smallp.style.display = "block";
+                plusicon.style.transform = `rotate(90deg)`;
+    
+    }}))
+
+    document.querySelectorAll(".plusicon").forEach(n => n.
+        addEventListener("click", () => {
+            if (plusicon !== "") {
+                plusicon.style.transform = `rotate(0deg)`;
+              } else {
+            
+                plusicon.style.transform = `rotate(90deg)`;
+    
+    }}))
