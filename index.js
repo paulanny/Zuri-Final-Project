dac34f7cdd48d8eaf3b45d489def78f5b677b628
paulanny <paulanimashaun20@gmail.com>
2022-11-08 03:45:31 +0100

    readytopush

--- index.js
+++ index.js
@@ -1,60 +1,49 @@
-// var Plus = document.getElementById('tablepss')
-// var Smallp = document.getElementById('smallp')
-// var PlusIcon = document.getElementById('plusiconn')
 
-// Plus.addEventListener("click", () => {
-//     Smallp.style.display = "block";
-//     PlusIcon.style.transform = `translateX(90deg)`;
-// })
 
-// const targetDiv = document.getElementById("tablepss");
-// const smallp = document.getElementById("smallp");
-// const plusicon = document.getElementById("plusiconn");
-// tablepss.onclick = function () {
-//   if (smallp.style.display !== "none") {
-//     smallp.style.display = "none";
-//     plusicon.style.transform = `rotate(0deg)`;
-//   } else {
-//     smallp.style.display = "block";
-//     plusicon.style.transform = `rotate(90deg)`;
-//   }
-// };
+const acc_btns = document.querySelectorAll('.accordionheader')
+const acc_body = document.querySelectorAll('.accordioncontent')
 
-const navMenu = document.querySelector(".accordlab");
-const plusicon = document.querySelector(".plusicon");
-const content = document.querySelector(".content");
 
-document.querySelectorAll(".accordlab").forEach((n) =>
-  n.addEventListener("click", () => {
-    if (content.style.display !== "none") {
-      content.style.display = "none";
-      plusicon.style.transform = `rotate(0deg)`;
-    } else {
-      content.style.display = "block";
-      content.style.transition = "max-height 0.5s ease";
-      plusicon.style.transform = `rotate(90deg)`;
+acc_btns.forEach((btn) => {
+    btn.addEventListener('click',  (e) => {
+        acc_body.forEach((acc) => {
+            if (
+                e.target.nextElementSibling !== acc &&
+                acc.classList.contains("active")
+            ) {
+                acc.classList.remove("active");
+                acc_btns.forEach((btn) => btn.classList.remove("active"));
+            }
+            
+        });
+        const panel = btn.nextElementSibling
+        panel.classList.toggle('active')
+        btn.classList.toggle('active')
+    });
+});
+
+// plusicon.forEach((plus) => {
+//   plus.addEventListener('click',  (e) => {
+//       acc_body.forEach((acc) => {
+//           if (
+//               e.target.nextElementSibling !== acc &&
+//               acc.classList.contains("active")
+//           ) {
+//               acc.classList.remove("active");
+//               acc_btns.forEach((btn) => btn.classList.remove("active"));
+//           }
+          
+//       });
+//       const panel = plus.nextElementSibling
+//       panel.classList.toggle('active')
+//       plus.classList.toggle('active')
+//   });
+// });
+
+window.onclick = (e) => {
+    if (!e.target.matches(".accordionheader")) {
+        acc_btns.forEach((btn) => btn.classList.remove("active"));  
+        acc_body.forEach((acc) => acc.classList.remove("active"));
     }
-  })
-);
-
-document.querySelectorAll(".plusicon").forEach((n) =>
-  n.addEventListener("click", () => {
-    if (plusicon.style.cursor !== "pointer") {
-      plusicon.style.transform = `rotate(0deg)`;
-    } else {
-      plusicon.style.transform = `rotate(90deg)`;
-    }
-  })
-);
-
-// document.querySelectorAll(".plusicon").forEach(n => n.
-//     addEventListener("click", () => {
-//         if (plusicon !== "") {
-//             plusicon.style.transform = `rotate(0deg)`;
-//           } else {
-
-//             plusicon.style.transform = `rotate(90deg)`;
-
-// }}))
-
+};
 
