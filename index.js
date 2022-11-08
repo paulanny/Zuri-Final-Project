42de4c3705e7dee4529aaf40fc98fa9d178d44e7
paulanny <paulanimashaun20@gmail.com>
2022-11-08 03:47:26 +0100

    final

--- index.js
+++ index.js
@@ -1,25 +1,21 @@
-
-
-const acc_btns = document.querySelectorAll('.accordionheader')
-const acc_body = document.querySelectorAll('.accordioncontent')
-
+const acc_btns = document.querySelectorAll(".accordionheader");
+const acc_body = document.querySelectorAll(".accordioncontent");
 
 acc_btns.forEach((btn) => {
-    btn.addEventListener('click',  (e) => {
-        acc_body.forEach((acc) => {
-            if (
-                e.target.nextElementSibling !== acc &&
-                acc.classList.contains("active")
-            ) {
-                acc.classList.remove("active");
-                acc_btns.forEach((btn) => btn.classList.remove("active"));
-            }
-            
-        });
-        const panel = btn.nextElementSibling
-        panel.classList.toggle('active')
-        btn.classList.toggle('active')
+  btn.addEventListener("click", (e) => {
+    acc_body.forEach((acc) => {
+      if (
+        e.target.nextElementSibling !== acc &&
+        acc.classList.contains("active")
+      ) {
+        acc.classList.remove("active");
+        acc_btns.forEach((btn) => btn.classList.remove("active"));
+      }
     });
+    const panel = btn.nextElementSibling;
+    panel.classList.toggle("active");
+    btn.classList.toggle("active");
+  });
 });
 
 // plusicon.forEach((plus) => {
@@ -32,7 +28,7 @@ acc_btns.forEach((btn) => {
 //               acc.classList.remove("active");
 //               acc_btns.forEach((btn) => btn.classList.remove("active"));
 //           }
-          
+
 //       });
 //       const panel = plus.nextElementSibling
 //       panel.classList.toggle('active')
@@ -41,9 +37,8 @@ acc_btns.forEach((btn) => {
 // });
 
 window.onclick = (e) => {
-    if (!e.target.matches(".accordionheader")) {
-        acc_btns.forEach((btn) => btn.classList.remove("active"));  
-        acc_body.forEach((acc) => acc.classList.remove("active"));
-    }
+  if (!e.target.matches(".accordionheader")) {
+    acc_btns.forEach((btn) => btn.classList.remove("active"));
+    acc_body.forEach((acc) => acc.classList.remove("active"));
+  }
 };
-
